cbacd588b269f7cdf723651e6e206af43b307ed4
Oluwajuwon Oyalude <oyaludejuwon@yahoo.com>
2023-01-13 13:32:11 +0100

    initial commit README file -  task details py file - python file

---
+++ README.md
@@ -0,0 +1,18 @@
+1.  Write a program to validates if a password is strong
+
+A password is strong if it contains at least 2 special characters, 
+at least 2 Uppercase letters, at least a lowercase letter, at least a number 
+and the minimum length is 8characters
+
+eg "Crossword" is NOT a strong password
+ "Cospis@1" is NOT a strong password
+
+"Cos@P!s895" is a STRONG password
+
+2.  For added complexity Question 1,  if the length is better 8 and 12, 
+print strong , if it is between 13 and 15, print very strong , if it is above 15, 
+print excellent
+
+3.  For added complexity to question 2, after validating a password, 
+ask if the user will like to validate another password, if user say yes, 
+program should start again automatically else program should terminate
---
+++ password validator.py
@@ -0,0 +1,50 @@
+
+word = input('enter a password: ')
+
+def check(word):
+    char = ['!','@','#','$','%','^','&','*','(',')']
+    count_up = 0
+    count_low = 0
+    count_special = 0
+    count_int = 0
+
+    for i,j,k,l in zip(word,word,word,word):
+        if i.islower():
+            count_low += 1
+            # if count_low
+        if j.isupper():
+            count_up += 1
+        if k in char:
+            count_special += 1
+        if l.isdigit():
+            count_int += 1
+                
+            
+    print(word)
+    print(count_low,count_up,count_special,count_int)
+    if count_special < 2 or count_up < 2 or count_low < 1 or count_int < 1 or len(word) < 8:
+        print(word + " is not a strong password")
+    else:
+        print(word + " is a strong password")
+   
+        if 8 <= len(word) <= 12:
+            print('strong')
+        elif 13 <= len(word) <= 15:
+            print('very strong')
+        elif len(word) > 15:
+            print('excellent')
+        
+    q = input('would you like to enter another password [y / n]: ')
+    while q == 'y':
+        if q != 'y':
+            break
+        else:
+            new_word = input('enter a new password: ')
+            check(new_word)
+            break
+        
+
+        
+            
+check(word)
+        
